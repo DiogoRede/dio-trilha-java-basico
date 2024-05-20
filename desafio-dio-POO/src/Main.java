ad752ee677a9d5d97ff0444b6082feb7026fedbb
DiogoRede <diogorede2021@gmail.com>
2024-05-20 14:42:09 -0300

    feat: adicionando novo desafio da DIO

---
+++ desafio-dio-POO/src/Main.java
@@ -0,0 +1,44 @@
+import br.com.dio.desafio.domain.Bootcamp;
+import br.com.dio.desafio.domain.Curso;
+import br.com.dio.desafio.domain.Dev;
+import br.com.dio.desafio.domain.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso spring");
+        curso2.setDescricao("descrição curso spring");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDiogo = new Dev();
+        devDiogo.setNome("Diogo");
+        devDiogo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Diogo:" + devDiogo.getConteudosInscritos());
+        devDiogo.progredir();
+        devDiogo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Diogo:" + devDiogo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Diogo:" + devDiogo.getConteudosConcluidos());
+        System.out.println("XP:" + devDiogo.calcularXp());
+
+    }
+}
